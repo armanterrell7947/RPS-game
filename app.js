1a4d5c4f7683bfdf184f2c39d752ef5889b31282
Arman Terrell <armanterrell7947@gmail.com>
2021-01-05 12:11:16 -0800

    up to date RPS project

--- app.js
+++ app.js
@@ -1,53 +1,90 @@
+//if you're helping me, thank you. Don't judge my code, I'm still learning. Help me solve the problem
+//OF WHY THE FUCK THIS WON'T BE RECOGNIZED IN MY HTML, I'M LOSING MY SHIT
+
+
 const choices = ['rock', 'paper', 'scissors'];
 
 let playerScore = 0;
 let computerScore = 0;
 
+//this is what sets the computers choice to be a random selection from the array
 function computerPlay () {
     let computerChoice = choices[Math.floor(Math.random() * choices.length)];
-    console.log(computerChoice);
+    return(computerChoice);
 
 }
 
 let computerSelection  = computerPlay();
-let playerSelection = computerPlay();
+let playerSelection = '';
+numberOfRounds = 0;
 
+let buttonList = document.querySelectorAll('.button');
+const para = document.querySelector('p');
+const scoreboard = document.querySelector('.scoreboard');
+
+/*
+for (let i = 0; i < btn.length ; i++){
+    btn[i] = btn.addEventListener('click', function(){
+        btn[i].forEach(function(btn){
+            alert('hello');
+}
+*/
+
+
+
+buttonList.forEach(function(button) {
+    buttonList.addEventListener('click', function(){
+        alert('hello');
+    } )
+});
+
+/* document.querySelectorAll('.button').addEventListener('click', function () {
+    alert('hello');
+  }, false);
+
+*/
+
+//  const highlightedItems = userList.querySelectorAll(".highlighted");
+
+/*highlightedItems.forEach(function(userItem) {
+  deleteUser(userItem);
+});
+*/
+
+/*
+btn.addEventListener('click', function(){
+    btn.forEach(function(btn){
+        alert('hello');
+    })
+})
+*/
 
 
 function playRound (playerSelection, computerSelection) {
     //sets winning parameters
+    
+
     if (playerSelection === 'rock' && computerSelection === 'scissors' || playerSelection==='scissors' && computerSelection==='paper' ||
     playerSelection === 'paper' && computerSelection === 'rock') {
         playerScore ++;
-        return `Hey! Congratulations! You won. Because you chose ${playerSelection} and the computer chose ${computerSelection}. Your 
+        numberOfRounds++;
+        para.textContent = `Hey! Congratulations! You won. Because you chose ${playerSelection} and the computer chose ${computerSelection}. Your 
         score is now: ${playerScore} and the computer's score is now ${computerScore}`;
+        
     }   //right here starts the tie outcome
         else if (playerSelection === computerSelection) {
-        return `Hey! It was a tie, try again!`
-    }   else {
+            endGame();
+        para.textContent =  `Hey! It was a tie, try again!`;
+            
+    }   else  {
         computerScore++;
-        return `Hey! Sorry! You lost! Because you chose ${playerSelection} and the computer chose ${computerSelection}, you were
+        numberOfRounds++;
+        para.textContent = `Hey! Sorry! You lost! Because you chose ${playerSelection} and the computer chose ${computerSelection}, you were
         hilariously defeated. The computer's score is now: ${computerScore} and your score is now ${playerScore}`;
+       
     }
+}
+
+
     
-}
-
-function game(game) {
-    roundsTotal = 6;
-    roundsPlayed = 0;
-    while (roundsPlayed < roundsTotal) {
-        playRound();
-        roundsPlayed ++
-    } 
-    if (roundsPlayed === 5) {
-        return `Refresh the page dammit!`;
-    }
-    if (playerScore < 5 || computerScore < 5){
-        continue
-    } else if (playerScore === 5) {
-        return 'You won!';
-    }   else {
-        return 'You lost!';
-    }
-}
-
+    
